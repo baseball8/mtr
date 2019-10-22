b34e11cc59fb5077e8b576fea6b259deed2fce34
baseball8 <yao.meng.hung@gmail.com>
2019-10-22 16:04:19 +0800

    import Agenda

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -1,83 +1,104 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-let _this, _navigation;
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+
 export class HomeScreen extends React.PureComponent {
-  static navigationOptions = ({navigation}) => {
-    return {
-      title: navigation.state?.params?.title || 'HomeScreen',
-      headerLeft: null,
-      headerRight: (
-        <T.Icon
-          iconSize={20}
-          color="black"
-          onPress={() => navigateTo(_navigation, 'ChooseScreen')}
-        />
-      ),
-    };
-  };
   state = {
-    data: null,
-    mounted: false,
-  };
-
-  componentDidMount() {
-    _trace();
-    this.mounted = true;
-    _this = this;
-    _navigation = this.props.navigation;
-    this.initStateData(() => {
-      this.autoRun();
-    });
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.navigation !== this.props.navigation)
-      _navigation = this.props.navigation;
-  }
-
-  medicationItem = ({item, index}) => {
-    return <T.Label text="藥事清單" />;
-  };
-
-  thingsItem = ({item, index}) => {
-    return <T.Label text="要事清單" />;
+    items: {},
   };
 
   render() {
-    let {data} = this.state;
-    // if (!data) return null
-    log(data, 'data in HomeScreen render() ');
     return (
-      <T.Screen>
-        <Calendar />
-        <T.Screen scrollable={true} backgroundColor="white">
-          <T.List data="222222222" renderItem={this.medicationItem} />
-        </T.Screen>
-
-        <T.Space />
-        <T.Screen scrollable={true} backgroundColor="white">
-          <T.List data="111" renderItem={this.thingsItem} />
-        </T.Screen>
-      </T.Screen>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems}
+        selected={'2019-10-18'}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+        rowHasChanged={this.rowHasChanged}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#666'},
+        //    '2017-05-09': {textColor: '#666'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
     );
   }
 
-  initStateData = onComplete => {
-    if (_navigation.state?.params) {
-      let {data} = _navigation.state?.params;
-      // _navigation.setParams({ title: '改為新標題' })
-      this.mounted &&
-        this.setState({data}, () => {
-          onComplete && onComplete();
-        });
-    } else {
-      onComplete && onComplete();
-    }
+  loadItems = day => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {
+        newItems[key] = this.state.items[key];
+      });
+      this.setState({
+        items: newItems,
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
   };
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-  autoRun = () => {};
+
+  renderItem = item => {
+    return (
+      <View style={[styles.item, {height: item.height}]}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
+  renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
+  rowHasChanged = (r1, r2) => {
+    return r1.name !== r2.name;
+  };
+
+  timeToString = time => {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  };
 }
-var styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+  },
+});
